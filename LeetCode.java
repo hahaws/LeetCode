76b66aeb08bcb6f932287aa7e3f7d904880eee48
hahaws <ws543284755@163.com>
2017-10-31 22:09:04 +0800

    17.10.31

--- LeetCode.java
+++ LeetCode.java
@@ -1,14 +1,9 @@
 public class LeetCode {
     public static void main(String[] args){
-        MergeTwoSortedLists so= new MergeTwoSortedLists();
-        ListNode l1=new ListNode(3);
-        ListNode l2=new ListNode(2);
-        ListNode re=so.mergeTwoLists(l1,l2);
-        System.out.println(""+re.val);
-        while (re!=null){
-            System.out.print(""+re.val);
-            re=re.next;
-        }
+        RemoveDuplicatesfromSortedArray so= new RemoveDuplicatesfromSortedArray();
+        int[] nums={1,1,1,2};
+        int res=so.removeDuplicates(nums);
+        System.out.println(""+res);
 
     }
 }
