ac8c50522a6a7cd0c86386514635a37865f79d83
hahaws <ws543284755@163.com>
2017-11-01 22:24:24 +0800

    17.11.1

--- LeetCode.java
+++ LeetCode.java
@@ -1,9 +1,10 @@
 public class LeetCode {
     public static void main(String[] args){
-        RemoveElement so= new RemoveElement();
-        int[] nums={3,2,2,3};
-        int res=so.removeElement(nums,2);
-        System.out.println(""+res);
+        ImplementStr so= new ImplementStr();
+        String h="";
+        String n="";
+        int i=so.strStr(h,n);
+        System.out.println(""+i);
 
     }
 }
