d35a1aaf4b92a16eda9f45539e857c006b583149
hahaws <ws543284755@163.com>
2017-11-02 14:07:23 +0800

    17.11.02

--- LeetCode.java
+++ LeetCode.java
@@ -2,7 +2,7 @@ public class LeetCode {
     public static void main(String[] args){
         SearchInsertPosition so= new SearchInsertPosition();
         int[] nums={1,3,5,6};
-        int i=so.searchInsert(nums,0);
+        int i=so.searchInsert(nums,9);
         System.out.println(""+i);
 
     }
--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -2,13 +2,25 @@
 You may assume no duplicates in the array.你可以假设数组中没有重复项*/
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        int len=nums.length;
+        //二分法；
+        int r = nums.length;
+        int l = 0;
+        while(l < r){
+            int mid = (l+r)/2;
+            if(nums[mid] < target)
+                l = mid +1;
+            else
+                r = mid;
+        }
+        return l;
+        //直接遍历效率太低了，远不如二分法；
+        /*int len=nums.length;
         int i=0;
         for(;i<len;i++){
             if(nums[i]>=target){
                 return i;
             }
         }
-        return i;
+        return i;*/
     }
 }
