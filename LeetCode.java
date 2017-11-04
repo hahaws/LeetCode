0d3c6bf2b2e5e8256cf38d1d4a5e4a1cfb3d8efc
hahaws <ws543284755@163.com>
2017-11-04 21:26:31 +0800

    17.11.04

--- LeetCode.java
+++ LeetCode.java
@@ -1,8 +1,9 @@
 public class LeetCode {
     public static void main(String[] args){
-        CountAndSay so= new CountAndSay();
-        String i=so.countAndSay(4);
-        System.out.println(i);
+        MaximumSubarray so= new MaximumSubarray();
+        int[] nums={-2,1,-3,4,-1,2,1,-5,4};
+        int i=so.maxSubArray(nums);
+        System.out.println(""+i);
 
     }
 }
