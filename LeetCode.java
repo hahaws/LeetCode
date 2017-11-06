29c4d101b13558c7fe31e7c31c72d6e1659c0838
hahaws <ws543284755@163.com>
2017-11-06 23:05:17 +0800

    17.11.06

--- LeetCode.java
+++ LeetCode.java
@@ -1,9 +1,11 @@
 public class LeetCode {
     public static void main(String[] args){
-        LengthofLastWord so= new LengthofLastWord();
-        String s=" hello ";
-        int i=so.lengthOfLastWord(s);
-        System.out.println(""+i);
+        PLusOne so= new PLusOne();
+        int[] nums={-1,9,-1};
+        int[] i=so.plusOne(nums);
+        for(int j=0;j<i.length;j++){
+            System.out.println(i[j]);
+        }
 
     }
 }
