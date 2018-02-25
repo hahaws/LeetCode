16926a7198eb797199d4c56c054c752fe53c7f62
hahaws <ws543284755@163.com>
2018-02-25 11:43:40 +0800

    18.02.25

--- LeetCode.java
+++ LeetCode.java
@@ -2,9 +2,17 @@ import java.io.File;
 
 public class LeetCode {
     public static void main(String[] args){
-        ClimbStairs so = new ClimbStairs();
-        int n=44;
-        int m=so.climbStairs(n);
-        System.out.println(""+m);
+        int[] nums1=new int[10];
+        nums1[0]=1;
+        nums1[1]=3;
+        nums1[2]=5;
+        nums1[3]=6;
+        int[] nums2=new int[3];
+        nums2[0]=0;
+        nums2[1]=4;
+        nums2[2]=5;
+        merge so=new merge();
+        so.merge(nums1,4,nums2,3);
+
     }
 }
