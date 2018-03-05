f09cfa21ac41067a2cb6d55429b98292fe6f443e
HAHA-ws <ws543284755@gmail.com>
2018-03-05 19:09:36 +0800

    18.3.5

--- SameTree.java
+++ SameTree.java
@@ -2,16 +2,16 @@
 //太笨了
 //还是要多看看多理解甚至多背背
 public class SameTree {
-    public boolean isSameTree(TreeNode p,TreeNode q){
-        if(p==null&&q==null){
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
             return true;
         }
-        if(p==null||q==null){
+        if (p == null || q == null) {
             return false;
         }
-        if(p.val==q.val){
-            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
-        }else {
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
             return false;
         }
     }
